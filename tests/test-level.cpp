28dc313704a329a6ac3276f1edfec812dbdd47f4
Ksenia-rgb <ksenialymova@mail.ru>
2026-08-11 18:57:03 +0300

    change test file, add test for set same level

--- tests/test-level.cpp
+++ tests/test-level.cpp
@@ -4,43 +4,50 @@
 
 #include <tecslog/tecslog.hpp>
 
-TEST_CASE(TestCorrectSetLevel)
+TEST_CASE(TecslogCorrectSetLevelTest)
 {
-  tecslog::init("logs/moday.log", tecslog::Level::WARNING);
+  tecslog::init("moday.log", tecslog::Level::WARNING);
   std::error_code code = tecslog::setLevel(tecslog::Level::INFO);
   return code.value() == 0;
 }
 
-TEST_CASE(TestCorrectSetStrLevel)
+TEST_CASE(TecslogCorrctSetSameLevelTest)
 {
-  tecslog::init("logs/moday.log", "WARNING");
+  tecslog::init("moday.log", "ERROR");
+  std::error_code code = tecslog::setLevel(tecslog::Level::ERROR);
+  return code.value() == 0;
+}
+
+TEST_CASE(TecslogCorrectSetStrLevelTest)
+{
+  tecslog::init("moday.log", "WARNING");
   std::error_code code = tecslog::setLevel("INFO");
   return code.value() == 0;
 }
 
-TEST_CASE(TestIncorrectSetStrLevel)
+TEST_CASE(TecslogIncorrectSetStrLevelTest)
 {
-  tecslog::init("logs/moday.log", "WARNING");
+  tecslog::init("moday.log", "WARNING");
   std::error_code code = tecslog::setLevel("INCORRECT");
   return code.value() != 0;
 }
 
-TEST_CASE(TestPrintPossibleLevels)
+TEST_CASE(TecslogPrintPossibleLevelsTest)
 {
-  tecslog::init("logs/moday.log", tecslog::Level::WARNING);
+  tecslog::init("moday.log", tecslog::Level::WARNING);
   
   std::stringstream sout;
   tecslog::printPossibleLevels(sout);
   return sout.str() == "0 INFO\n1 WARNING\n2 ERROR";
 }
 
-TEST_CASE(TestCorrectParseLevel)
+TEST_CASE(TecslogCorrectParseLevelTest)
 {
   std::optional< tecslog::Level > level = tecslog::parseLevel("ERROR");
   return level.has_value() && level.value() == tecslog::Level::ERROR;
 }
 
-TEST_CASE(TestIncorrectParseLevel)
+TEST_CASE(TecslogIncorrectParseLevelTest)
 {
   std::optional< tecslog::Level > level = tecslog::parseLevel("INCORRECT");
   return !level.has_value();
