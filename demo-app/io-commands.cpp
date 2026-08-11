ae626b6560e01b31520c3ee797d187198473c765
Ksenia-rgb <ksenialymova@mail.ru>
2026-08-11 17:29:59 +0300

    fix output in demo commands

--- demo-app/io-commands.cpp
+++ demo-app/io-commands.cpp
@@ -33,8 +33,8 @@ std::error_code demo::silenceCommand(std::istream& in, std::ostream& out, LogInf
   {
     return std::make_error_code(std::errc::invalid_argument);
   }
-  LogInfo::silence_level = level;
-  out << "[OK] silence level is specified on " << str_level;
+  LogInfo::silence_level = level.value();
+  out << "OK: silence level is specified on " << level.value();
   return {};
 }
 
@@ -47,12 +47,13 @@ std::error_code demo::defaultCommand(std::istream& in, std::ostream& out)
     return code;
   }
 
-  code = tecslog::setLevel(str_level);
-  if (code)
+  auto level = tecslog::parseLevel(str_level);
+  if (!level.has_value())
   {
-    return code;
+    return std::make_error_code(std::errc::invalid_argument);
   }
-  out << "OK: default level is specified on " << str_level;
+  tecslog::setLevel(level.value());
+  out << "OK: default level is specified on " << level.value();
   return {};
 }
 
@@ -92,9 +93,9 @@ std::error_code demo::logCommand(std::istream& in, std::ostream& out, LogInfo& i
 std::error_code demo::helpCommand(std::istream& in, std::ostream& out)
 {
   out << "Commands:\n"
-    << "\t1 silence <level>\t= Specify silence log level\n"
-    << "\t2 default <level>\t= Specify default log level, logs with a lower level will not be recorded\n"
-    << "\t3 log <message> <level>\t= Log message with level\n"
+    << "\t1 default <level>\t= Specify default log level, logs with a lower level will not be recorded\n"
+    << "\t2 silence <level>\t= Specify silence log level\n"
+    << "\t3 log <message> [level]\t= Log message with optional level\n"
     << "\tThe <level> may not be specified if it was specified by the `silence` command.\n"
     << "\tIn such case log will have <level>, specified as `silence` <level>\n"
     << "\tIf you did not specify <level> and not use `silence`, command is invalid\n\n";
